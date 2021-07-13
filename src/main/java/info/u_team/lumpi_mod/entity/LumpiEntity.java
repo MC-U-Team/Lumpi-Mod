d01b69d994c019f8db81fc4480b15c9e1ad92d92
HyCraftHD <hycrafthd@live.de>
2021-07-13 15:13:32 +0200

    Fix lumpi spawning and mating

--- src/main/java/info/u_team/lumpi_mod/entity/LumpiEntity.java
+++ src/main/java/info/u_team/lumpi_mod/entity/LumpiEntity.java
@@ -1,13 +1,19 @@
 package info.u_team.lumpi_mod.entity;
 
+import java.util.UUID;
+
+import info.u_team.lumpi_mod.init.LumpiModEntityTypes;
+import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.network.IPacket;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 public class LumpiEntity extends WolfEntity {
@@ -37,6 +43,37 @@ public class LumpiEntity extends WolfEntity {
 		}
 		
 		getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(6);
+	}
+	
+	@Override
+	public LumpiEntity createChild(ServerWorld world, AgeableEntity mate) {
+		final LumpiEntity lumpiEntity = LumpiModEntityTypes.LUMPI.get().create(world);
+		final UUID uuid = this.getOwnerId();
+		if (uuid != null) {
+			lumpiEntity.setOwnerId(uuid);
+			lumpiEntity.setTamed(true);
+		}
+		return lumpiEntity;
+	}
+	
+	@Override
+	public boolean canMateWith(AnimalEntity otherAnimal) {
+		if (otherAnimal == this) {
+			return false;
+		} else if (!this.isTamed()) {
+			return false;
+		} else if (!(otherAnimal instanceof LumpiEntity)) {
+			return false;
+		} else {
+			final LumpiEntity lumpiEntity = (LumpiEntity) otherAnimal;
+			if (!lumpiEntity.isTamed()) {
+				return false;
+			} else if (lumpiEntity.isEntitySleeping()) {
+				return false;
+			} else {
+				return isInLove() && lumpiEntity.isInLove();
+			}
+		}
 	}
 	
 	@Override
