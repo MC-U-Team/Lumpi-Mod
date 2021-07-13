8a2ac75ca6d171fe5c30144a270c5adb977624ca
HyCraftHD <hycrafthd@live.de>
2021-07-13 14:56:46 +0200

    Add spawn egg item class

--- src/main/java/info/u_team/lumpi_mod/init/LumpiModItems.java
+++ src/main/java/info/u_team/lumpi_mod/init/LumpiModItems.java
@@ -1,10 +1,10 @@
 package info.u_team.lumpi_mod.init;
 
 import info.u_team.lumpi_mod.LumpiMod;
+import info.u_team.lumpi_mod.item.LumpiSpawnEggItem;
 import info.u_team.u_team_core.item.USpawnEggItem;
 import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import net.minecraft.item.Item;
-import net.minecraft.item.Item.Properties;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,7 +13,7 @@ public class LumpiModItems {
 	
 	public static final CommonDeferredRegister<Item> ITEMS = CommonDeferredRegister.create(ForgeRegistries.ITEMS, LumpiMod.MODID);
 	
-	public static final RegistryObject<USpawnEggItem> LUMPI_SPAWN_EGG = ITEMS.register("lumpi_spawn_egg", () -> new USpawnEggItem(LumpiModItemsGroups.GROUP, new Properties(), LumpiModEntities.LUMPI, 0x8a4127, 0xc46c6c));
+	public static final RegistryObject<USpawnEggItem> LUMPI_SPAWN_EGG = ITEMS.register("lumpi_spawn_egg", LumpiSpawnEggItem::new);
 	
 	public static void registerMod(IEventBus bus) {
 		ITEMS.register(bus);
---
+++ src/main/java/info/u_team/lumpi_mod/item/LumpiSpawnEggItem.java
@@ -0,0 +1,14 @@
+package info.u_team.lumpi_mod.item;
+
+import info.u_team.lumpi_mod.init.LumpiModEntities;
+import info.u_team.lumpi_mod.init.LumpiModItemsGroups;
+import info.u_team.u_team_core.item.USpawnEggItem;
+import net.minecraft.item.Rarity;
+
+public class LumpiSpawnEggItem extends USpawnEggItem {
+	
+	public LumpiSpawnEggItem() {
+		super(LumpiModItemsGroups.GROUP, new Properties().rarity(Rarity.UNCOMMON), LumpiModEntities.LUMPI, 0x8a4127, 0xd1b4b4);
+	}
+	
+}
