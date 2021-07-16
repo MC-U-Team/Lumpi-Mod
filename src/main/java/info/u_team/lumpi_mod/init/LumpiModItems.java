563407133db9f57312bf3503195c0b3077e6b41c
HyCraftHD <hycrafthd@live.de>
2021-07-17 01:13:52 +0200

    Make spawn egg color a mit more vibrant

--- src/main/java/info/u_team/lumpi_mod/init/LumpiModItems.java
+++ src/main/java/info/u_team/lumpi_mod/init/LumpiModItems.java
@@ -17,7 +17,7 @@ public class LumpiModItems {
 	
 	public static final RegistryObject<USpawnEggItem> LUMPI_SPAWN_EGG = ITEMS.register("lumpi_spawn_egg", () -> new LumpiSpawnEggItem(LumpiModEntityTypes.LUMPI, Rarity.UNCOMMON, 0x8a4127, 0xd1b4b4));
 	public static final RegistryObject<USpawnEggItem> LOADED_LUMPI_SPAWN_EGG = ITEMS.register("loaded_lumpi_spawn_egg", () -> new LumpiSpawnEggItem(LumpiModEntityTypes.LOADED_LUMPI, Rarity.RARE, 0x2e160d, 0xd1b4b4));
-	public static final RegistryObject<USpawnEggItem> STEEL_LUMPI_SPAWN_EGG = ITEMS.register("steel_lumpi_spawn_egg", () -> new LumpiSpawnEggItem(LumpiModEntityTypes.STEEL_LUMPI, Rarity.EPIC, 0x292727, 0xd1b4b4));
+	public static final RegistryObject<USpawnEggItem> STEEL_LUMPI_SPAWN_EGG = ITEMS.register("steel_lumpi_spawn_egg", () -> new LumpiSpawnEggItem(LumpiModEntityTypes.STEEL_LUMPI, Rarity.EPIC, 0x261c1c, 0x615e5e));
 	
 	public static final RegistryObject<HotdogItem> HOTDOG = ITEMS.register("hotdog", HotdogItem::new);
 	
